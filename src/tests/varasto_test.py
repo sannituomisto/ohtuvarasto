9909b8ed3a213823d402a404842945dbfdbef4a3
Sanni Tuomisto <101722915+sannituomisto@users.noreply.github.com>
2022-11-01 16:49:17 +0200

    Update varasto_test.py

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -11,7 +11,7 @@ class TestVarasto(unittest.TestCase):
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_uudella_varastolla_oikea_tilavuus(self):
-        self.assertAlmostEqual(self.varasto.tilavuus, 12)
+        self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
     def test_uudella_varastolla_neg_tilavuus(self):
         self.varasto = Varasto(-10)
